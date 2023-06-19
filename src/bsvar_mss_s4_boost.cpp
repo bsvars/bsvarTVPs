980c17834dc293d20d47e980a4f26396ecae5f4d
Tomasz Woźniak <wozniak.tom@pm.me>
2023-06-19 17:04:06 +1000

    shameful debugging

--- src/bsvar_mss_s4_boost.cpp
+++ src/bsvar_mss_s4_boost.cpp
@@ -22,6 +22,9 @@ Rcpp::List bsvar_mss_s4_boost_cpp (
     const int                     thin = 100  // introduce thinning
 ) {
   // // Progress bar setup
+  
+  Rcout << "check 1" << endl;
+  
   vec prog_rep_points = arma::round(arma::linspace(0, SS, 50));
   Rcout << "**************************************************|" << endl;
   Rcout << "bsvarTVPs: Bayesian Structural VARs with          |" << endl;
@@ -76,7 +79,11 @@ Rcpp::List bsvar_mss_s4_boost_cpp (
   
   int   s = 0;
   
+  Rcout << "check 2" << endl;
+  
   for (int ss=0; ss<SS; ss++) {
+    
+    Rcout << "   iteration: " << ss << endl;
     
     // Increment progress bar
     if (any(prog_rep_points == ss)) p.increment();
@@ -84,19 +91,33 @@ Rcpp::List bsvar_mss_s4_boost_cpp (
     if (ss % 200 == 0) checkUserInterrupt();
     
     // sample aux_xi
+    
+    Rcout << "   aux_xi" << endl;
+    
     mat E = Y - aux_A * X;
+    Rcout << "   aux_xi1" << endl;
     aux_xi_tmp        = aux_xi;
+    Rcout << "   aux_xi2" << endl;
     try {
-      aux_xi_tmp      = sample_Markov_process_mss(aux_xi, E, aux_B, aux_sigma, aux_PR_TR, aux_pi_0);
+      aux_xi_tmp      = sample_Markov_process_mss(aux_xi, E, aux_B, aux_sigma, aux_PR_TR, aux_pi_0, true);
     } catch (...) {
       acceptance_count(0)++;
     }
+    Rcout << "   aux_xi3" << endl;
     aux_xi            = aux_xi_tmp;
     
+    
+    
     // sample aux_PR_TR
+    
+    Rcout << "   aux_PR_TR" << endl;
+    
     bsvars::sample_transition_probabilities(aux_PR_TR, aux_pi_0, aux_xi, prior);
     
     // sample aux_hyper
+    
+    Rcout << "   aux_hyper" << endl;
+    
     aux_hyper_tmp     = aux_hyper;
     try {
       aux_hyper_tmp   = sample_hyperparameters_mss_s4_boost( aux_hyper, aux_B, aux_A, VB, aux_SL, prior);
@@ -106,6 +127,9 @@ Rcpp::List bsvar_mss_s4_boost_cpp (
     aux_hyper         = aux_hyper_tmp;
     
     // sample aux_B
+    
+    Rcout << "   aux_B" << endl;
+    
     BSL     = List::create(
       _["aux_B"]      = aux_B,
       _["aux_SL"]     = aux_SL
@@ -119,6 +143,9 @@ Rcpp::List bsvar_mss_s4_boost_cpp (
     aux_SL            = as<imat>(BSL["aux_SL"]);
     
     // sample aux_A
+    
+    Rcout << "   aux_A" << endl;
+    
     aux_A_tmp         = aux_A;
     try {
       aux_A_tmp       = sample_A_heterosk1_mss_boost(aux_A, aux_B, aux_xi, aux_hyper, aux_sigma, Y, X, prior);
@@ -126,6 +153,9 @@ Rcpp::List bsvar_mss_s4_boost_cpp (
       acceptance_count(3)++;
     }
     aux_A             = aux_A_tmp;
+    
+    
+    Rcout << "   save" << endl;
     
     if (ss % thin == 0) {
       posterior_B(s)                = aux_B;
