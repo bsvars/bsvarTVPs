c8e0805cf70be8ca2d0970b31ad9244db33d9467
Tomasz Woźniak <wozniak.tom@pm.me>
2024-11-14 19:12:02 +1100

    get rid of generics

--- R/bsvarTVPTOOLs.R
+++ R/bsvarTVPTOOLs.R
@@ -1,41 +1,5 @@
 
 
-#' @title Computes regime-specific impulse response functions of the dependent variables 
-#' to the structural shocks
-#'
-#' @description Computes regime-specific impulse response functions of the dependent variables 
-#' to the structural shocks for SVAR models with Markov-switching structural matrix with \code{M} regimes
-#' 
-#' 
-#' @param posterior an object of class of class PosteriorBSVARSVMSTVI, 
-#' PosteriorBSVARSVMS, or PosteriorBSVARSVTVI with random draws from the posterior 
-#' distribution of the Structural model with Markov-switching structural matrix
-#' @param horizon a positive integer specifying the forecasting horizon for the impulse responses
-#' @param standardise a logical value. If \code{TRUE}, the impulse responses are standardised 
-#' so that the variables' own shocks at horizon 0 are equal to 1. Otherwise, the parameter estimates 
-#' determine this magnitude.
-#' 
-#' @return An \code{N x N x horizon x M x S} array containing the posterior draws 
-#' of the impulse responses 
-#'
-#' @author Tomasz Woźniak \email{wozniak.tom@pm.me}
-#' 
-#' @references
-#' Camehl, A. & Woźniak, T. (2022) What do Data Say About Time-Variation in 
-#' Monetary Policy Shock Identification?
-#' 
-#' @export
-compute_impulse_responses <- function(posterior, horizon, standardise = FALSE) {
-  
-  # check arguments
-  stopifnot("Argument posterior must be of class PosteriorBSVARSVMSTVI, PosteriorBSVARSVMS, or PosteriorBSVARSVTVI." = substr(class(posterior), 1, 16) == "PosteriorBSVARSV")
-  stopifnot("Argument horizon must be a positive integer number." = horizon >= 1 & horizon %% 1 == 0)
-  
-  # call method
-  UseMethod("compute_impulse_responses", posterior)
-}
-
-
 #' @inherit compute_impulse_responses
 #' @method compute_impulse_responses PosteriorBSVARSVMSTVI
 #' @inheritParams compute_impulse_responses
@@ -188,120 +152,6 @@ compute_impulse_responses_by_components <- function(ir_posterior, TVI_posterior,
 }
 
 
-#' @title Computes posterior draws of regime probabilities
-#'
-#' @description Each of the draws from the posterior estimation of a model is transformed into
-#' a draw from the posterior distribution of the regime probabilities. These represent either
-#' the realisations of the regime indicators, when \code{type = "realized"}, filtered probabilities,
-#' when \code{type = "filtered"}, forecasted regime probabilities, when \code{type = "forecasted"},
-#' or the smoothed probabilities, when \code{type = "smoothed"}, .
-#' 
-#' @param posterior posterior estimation outcome of regime-dependent heteroskedastic models 
-#' - an object of either of the classes: PosteriorBSVARSVMSTVI, or PosteriorBSVARSVMS.
-#' @param Y a \code{N x T} matrix of dependent variables
-#' @param X a \code{K x T} matrix of lagged variables
-#' @param type one of the values \code{"realized"}, \code{"filtered"}, \code{"forecasted"}, or \code{"smoothed"}
-#' denoting the type of probabilities to be computed.
-#' 
-#' @return An object of class PosteriorRegimePr, that is, an \code{MxTxS} array with attribute PosteriorRegimePr 
-#' containing \code{S} draws of the regime probabilities.
-#'
-#' @author Tomasz Woźniak \email{wozniak.tom@pm.me}
-#' 
-#' @references 
-#' Song, Y., and Woźniak, T., (2021) Markov Switching. \emph{Oxford Research Encyclopedia of Economics and Finance}, Oxford University Press, \doi{https://doi.org/10.1093/acrefore/9780190625979.013.174}.
-#'  
-#' @export
-compute_regime_probabilities <- function(posterior, Y, X, type = c("realized", "filtered", "forecasted", "smoothed")) {
-  
-  stopifnot("Argument posterior must be of class PosteriorBSVARSVMSTVI or PosteriorBSVARSVMS." = substr(class(posterior), 1, 18) == "PosteriorBSVARSVMS")
-  
-  type          = match.arg(type)
-  
-  posteriors    = posterior$posterior
-  # Y             = posterior$last_draw$data_matrices$Y
-  # X             = posterior$last_draw$data_matrices$X
-  
-  if (type == "realized") {
-    probs       = posteriors$xi
-  } else {
-    if (type == "filtered") {
-      forecasted  = FALSE
-      smoothed    = FALSE
-    } else if (type == "forecasted") {
-      forecasted  = TRUE
-      smoothed    = FALSE
-    } else if (type == "smoothed") {
-      forecasted  = FALSE
-      smoothed    = TRUE
-    }
-    
-    probs       = .Call(`_bsvarTVPs_bsvarTVPs_filter_forecast_smooth`, posteriors, Y, X, forecasted, smoothed)
-  }
-  
-  class(probs)  = "PosteriorRegimePr"
-  
-  return(probs)
-}
-
-
-#' @title Computes posterior draws of dependent variables' fitted values
-#'
-#' @description Each of the draws from the posterior estimation of a model is transformed into
-#' a draw from the posterior distribution of the fitted values. 
-#' 
-#' @param posterior posterior estimation outcome - an object of either of the classes: 
-#' PosteriorBSVARSVMSTVI, PosteriorBSVARSVMS, or PosteriorBSVARSVTVI.
-#' @param X a \code{K x T} matrix of vector autoregressive regressors.
-#' 
-#' @return An object of class PosteriorFitted, that is, an \code{NxTxS} array with attribute PosteriorFitted 
-#' containing \code{S} draws of the fitted values.
-#'
-#' @author Tomasz Woźniak \email{wozniak.tom@pm.me}
-#' 
-#' @export
-compute_fitted_values <- function(posterior, X) {
-  
-  stopifnot("Argument posterior must contain estimation output." = any(class(posterior)[1] == c("PosteriorBSVARSVMSTVI", "PosteriorBSVARSVMS", "PosteriorBSVARSVTVI")))
-  
-  posterior_A     = posterior$posterior$A
-  # X               = posterior$last_draw$data_matrices$X
-  
-  fv              = .Call(`_bsvarTVPs_bsvarTVPs_fitted_values`, posterior_A, X)
-  class(fv)       = "PosteriorFitted"
-  
-  return(fv)
-}
-
-
-
-
-
-#' @title Computes posterior draws of structural shocks
-#'
-#' @description Each of the draws from the posterior estimation of a model is transformed into
-#' a draw from the posterior distribution of the structural shocks. 
-#' 
-#' @param posterior posterior estimation outcome - an object of either of the classes: 
-#' PosteriorBSVARSVMSTVI, PosteriorBSVARSVMS, or PosteriorBSVARSVTVI.
-#' @param Y a \code{N x T} matrix od dependent variables
-#' @param X a \code{K x T} matrix of vector autoregressive regressors.
-#' 
-#' @return An object of class PosteriorShocks, that is, an \code{NxTxS} array with attribute PosteriorShocks 
-#' containing \code{S} draws of the structural shocks.
-#'
-#' @author Tomasz Woźniak \email{wozniak.tom@pm.me}
-#' 
-#' @export
-compute_structural_shocks <- function(posterior, Y, X) {
-  
-  stopifnot("Argument posterior must contain estimation output." = any(class(posterior)[1] == c("PosteriorBSVARSVMSTVI", "PosteriorBSVARSVMS", "PosteriorBSVARSVTVI")))
-  
-  # call method
-  UseMethod("compute_structural_shocks", posterior)
-}
-
-
 
 
 #' @inherit compute_structural_shocks
@@ -309,10 +159,12 @@ compute_structural_shocks <- function(posterior, Y, X) {
 #' @inheritParams compute_structural_shocks
 #' 
 #' @export
-compute_structural_shocks.PosteriorBSVARSVMSTVI <- function(posterior, Y, X) {
+compute_structural_shocks.PosteriorBSVARSVMSTVI <- function(posterior) {
   
   posterior_B     = posterior$posterior$B
   posterior_A     = posterior$posterior$A
+  Y               = posterior$last_draw$data_matrices$Y
+  X               = posterior$last_draw$data_matrices$X
   
   posterior_xi    = posterior$posterior$xi
   ss              = .Call(`_bsvarTVPs_bsvarTVPs_structural_shocks`, posterior_B, posterior_A, posterior_xi, Y, X)
@@ -327,10 +179,12 @@ compute_structural_shocks.PosteriorBSVARSVMSTVI <- function(posterior, Y, X) {
 #' @inheritParams compute_structural_shocks
 #' 
 #' @export
-compute_structural_shocks.PosteriorBSVARSVMS <- function(posterior, Y, X) {
+compute_structural_shocks.PosteriorBSVARSVMS <- function(posterior) {
   
   posterior_B     = posterior$posterior$B
   posterior_A     = posterior$posterior$A
+  Y               = posterior$last_draw$data_matrices$Y
+  X               = posterior$last_draw$data_matrices$X
   
   posterior_xi    = posterior$posterior$xi
   ss              = .Call(`_bsvarTVPs_bsvarTVPs_structural_shocks`, posterior_B, posterior_A, posterior_xi, Y, X)
@@ -345,10 +199,13 @@ compute_structural_shocks.PosteriorBSVARSVMS <- function(posterior, Y, X) {
 #' @inheritParams compute_structural_shocks
 #' 
 #' @export
-compute_structural_shocks.PosteriorBSVARSVTVI <- function(posterior, Y, X) {
+compute_structural_shocks.PosteriorBSVARSVTVI <- function(posterior) {
   
   posterior_B     = posterior$posterior$B
   posterior_A     = posterior$posterior$A
+  Y               = posterior$last_draw$data_matrices$Y
+  X               = posterior$last_draw$data_matrices$X
+  
   ss              = .Call(`_bsvarTVPs_bsvars_structural_shocks`, posterior_B, posterior_A, Y, X)
   class(ss)       = "PosteriorShocks"
   
@@ -820,47 +677,4 @@ compute_conditional_cov.PosteriorBSVARTVI <- function(posterior, moment = c("cov
   return(covs_out)
 }
 
-
-
-
-#' @title Computes posterior draws of structural shock conditional standard deviations
-#'
-#' @description Each of the draws from the posterior estimation of a model is transformed into
-#' a draw from the posterior distribution of the structural shock conditional standard deviations. 
-#' 
-#' @param posterior posterior estimation outcome - an object of either of the classes: 
-#' PosteriorBSVARSVMSTVI, PosteriorBSVARSVTVI, PosteriorBSVARSVMS, PosteriorBSVARMSTVI, 
-#' PosteriorBSVARTVI, or PosteriorBSVARMS
-#' 
-#' @return An object of class PosteriorSigma, that is, an \code{NxTxS} array with attribute PosteriorSigma 
-#' containing \code{S} draws of the structural shock conditional standard deviations.
-#'
-#' @author Tomasz Woźniak \email{wozniak.tom@pm.me}
-#' 
-#' @export
-compute_conditional_sd <- function(posterior) {
-  
-  stopifnot("Argument posterior must contain estimation output from the estimate 
-            function for a SVAR model." = any(class(posterior)[1] == 
-                                                         c("PosteriorBSVARSVMSTVI", 
-                                                           "PosteriorBSVARSVTVI", 
-                                                           "PosteriorBSVARSVMS", 
-                                                           "PosteriorBSVARMSTVI",
-                                                           "PosteriorBSVARTVI",
-                                                           "PosteriorBSVARMS")))
-  
-  Y     = posterior$last_draw$data_matrices$Y
-  N     = nrow(Y)
-  T     = ncol(Y)
-  
-  if (any(class(posterior)[1] == c("PosteriorBSVARMSTVI", "PosteriorBSVARTVI", "PosteriorBSVARMS"))) {
-    posterior_sigma       = matrix(1, N, T)
-    message("The structural shocks are homoskedastic. Returning an NxT matrix of conditional sd all equal to 1.")
-  } else {
-    posterior_sigma       = posterior$posterior$sigma
-  }
-  class(posterior_sigma)  = "PosteriorSigma"
-  
-  return(posterior_sigma)
-}
 
