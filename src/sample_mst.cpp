ff454a537aeb7e513b64ef822b17d6ecd5efb87d
Tomasz Woźniak <wozniak.tom@pm.me>
2025-11-07 12:28:02 +1100

    simplify sampling lambda #22

--- src/sample_mst.cpp
+++ src/sample_mst.cpp
@@ -12,9 +12,9 @@ arma::mat sample_lambda_ms (
     const arma::mat&    aux_df,     // NxM
     const arma::mat&    aux_xi      // MxT
 ) {
+  
   const int N           = aux_df.n_rows;
   const int T           = aux_xi.n_cols;
-  vec       Tm          = sum(aux_xi, 1);  
   
   mat       nu_lambda   = aux_df + 1;
   mat       s_lambda    = aux_df - 1;
@@ -22,10 +22,10 @@ arma::mat sample_lambda_ms (
   
   for (int n=0; n<N; n++) {
     for (int t=0; t<T; t++) {
-  
+      
       int     m         = aux_xi.col(t).index_max();
-      double  draw      = as<double>(Rcpp::rchisq(1, nu_lambda(n, m)));
-      aux_lambda(n, t)  = s_lambda(n, t) / draw;
+      double  draw      =  chi2rnd(nu_lambda(n, m));
+      aux_lambda(n, t)  = s_lambda(n, m) / draw;
   
     }
   }
@@ -80,6 +80,7 @@ Rcpp::List sample_df_ms (
   // is negligible for alpha computation
   for (int n = 0; n < N; n++){
     for (int m = 0; m < M; m++){
+      
       aux_df_star(n, m)       = RcppTN::rtn1( aux_df(n, m), adaptive_scale(n), 2, R_PosInf );
       
       uvec    aux_xi_m        = find(aux_xi.row(m));
@@ -88,7 +89,7 @@ Rcpp::List sample_df_ms (
       
       double  kernel_ratio     = exp( log_kernel_df_ms_nm(aux_df_star(n, m), lambda, prior_df_a) - log_kernel_df_ms_nm(aux_df(n, m), lambda, prior_df_a) );
       kernel_ratio            *= RcppTN::dtn1(aux_df_star(n, m), aux_df(n, m), adaptive_scale(n), 2, R_PosInf) / RcppTN::dtn1(aux_df(n, m), aux_df_star(n, m), adaptive_scale(n), 2, R_PosInf);
-             
+      
       if ( kernel_ratio < 1 ) alpha(n, m) = kernel_ratio;
       if ( R::runif(0, 1) < alpha(n, m) ) {
         aux_df(n, m) = aux_df_star(n, m);
